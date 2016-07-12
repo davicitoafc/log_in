93acc851016654c8c85ec4bb1b52855c0c368b91
David Cisneros <d.cisneros849@gmail.com>
2016-07-12 11:04:45 -0400

    added jquery to add form

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ $(function () {
 
 $(".button").on('click', function() {
   console.log("Pressing Button");
-  
+
+var form = $("<form>", { method: "GET" });
+
+  form.append("<input>", { type: "text"});
+  form.addClass("form_style");
+
+  $( ".main_container" ).prepend(form);
+
   });
 });
